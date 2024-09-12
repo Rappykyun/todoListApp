34c260a6622a9a9c231292bd672dd7449422baf3
Rappykyun <rakuichijo155@gmail.com>
2024-09-12 12:01:18 +0800

    configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true
   },
   devServer: {
     port: 8000,
